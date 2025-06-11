03e46d39357dbce0f9d00f33a573be5b64e9d08e
Domitori <domitorione@gmail.com>
2025-06-12 01:45:12 +0300

    12

---
+++ src/app/products/page.tsx
@@ -0,0 +1,17 @@
+import CategorySection from "@/components/categories/CategorySection";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+
+export default function ProductsPage() {
+    return (
+
+        <>  
+        
+        <Header />
+            <CategorySection />
+        <Footer />
+        
+        </>
+        
+    )
+}
